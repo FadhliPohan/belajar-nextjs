bfebf6094710072e16eb27b686dc7119c11cf241
FadhliPohan <muhammadfadly.mfd@gmail.com>
2024-01-25 09:41:25 +0700

    FEAT(TUGAS-H9):Pembuatan API Routes

--- learn-nextjs/pages/api/hello.js
+++ learn-nextjs/pages/api/hello.js
@@ -1,5 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  // res
+  //   .status(200)
+  //   .json({ name: "John Doe", kelas: "2b", alamat: "K3" });
+
+  try {
+    const response = await (await fetch("https://dummyjson.com/users")).json();
+    res.status(200).json({ ...response });
+  } catch (error) {
+    console.log(error);
+  }
 }
--- learn-nextjs/pages/index.js
+++ learn-nextjs/pages/index.js
@@ -6,13 +6,27 @@ import Footer from "@/components/footer";
 import Header from "@/components/header";
 import Content from "@/components/content";
 import Layout from "@/layout";
+import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  useEffect(() => {
+    fetch("api/hello")
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
     <>
-      <Layout metaTitle="Home" metaDescription="Ini adalah halaman home, informasi dapat anda lihat disini!">
+      <Layout
+        metaTitle="Home"
+        metaDescription="Ini adalah halaman home, informasi dapat anda lihat disini!"
+      >
         <p>Makan Apa Pagi Ini</p>
       </Layout>
     </>
