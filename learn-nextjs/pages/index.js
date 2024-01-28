b67d3cc85bb1cf68b116adb7686b43fe087925e9
FadhliPohan <muhammadfadly.mfd@gmail.com>
2024-01-28 07:27:18 +0700

    Feat(Tugas-10): Membuat optimalisasi Dynamic import dan Image

--- learn-nextjs/pages/index.js
+++ learn-nextjs/pages/index.js
@@ -1,13 +1,11 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
-import Footer from "@/components/footer";
-import Header from "@/components/header";
-import Content from "@/components/content";
-import Layout from "@/layout";
-import { useEffect } from "react";
 
+import { Inter } from "next/font/google";
+
+import { useEffect } from "react";
+import dynamic from "next/dynamic";
+import Image from "next/image";
+
+const LayoutComponent = dynamic(()=>import("@/layout"))
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
@@ -23,12 +21,13 @@ export default function Home() {
   }, []);
   return (
     <>
-      <Layout
+      <LayoutComponent
         metaTitle="Home"
         metaDescription="Ini adalah halaman home, informasi dapat anda lihat disini!"
       >
-        <p>Makan Apa Pagi Ini</p>
-      </Layout>
+        <Image src="/ultramen.jpg" width={200} height={200} alt="ultramen"/>
+        <img src="/ultramen.jpg" style={{  width:200,height:200 }} alt="ultramen"/>
+      </LayoutComponent>
     </>
   );
 }
